06b2e166b11c29f5dfbddda78c92c45f0172d75b
nesquikm <drapolyuk@gmail.com>
2023-10-23 21:24:49 +0400

    feat: add melos

---
+++ scripts/clean.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Remove build files generated code
+find . -type f -name "*_generated.*" -delete
+find . -type f -name "*.freezed.dart" -delete
+find . -type f -name "*.g.dart" -delete
+find . -type f -name "*.gen.dart" -delete
+find . -type f -name "*.reflectable.dart" -delete
+find . -type f -name "*.module.dart" -delete
+
+# Remove DI config
+rm -rf lib/di/di.config.dart
+
+# # Secrets
+# rm -f android/crystal.keystore
+# rm -f android/key.properties
+# rm -f android/fastlane/GooglePlayServiceAccount.json
+# rm -f ios/fastlane/AuthKey_L4N29B6Z42.p8
+# rm -f fastlane/FirebaseAPIKey.json
+# rm -f fastlane/FirebaseADKey.json
+# rm -f secrets/ios-provisioning-key*
+
+# Remove changelog
+# rm -rf fastlane/changelog.txt
+# rm -rf android/fastlane/metadata/android/
+# rm -rf packages/ui_components_lib/example/assets/git_log.txt
